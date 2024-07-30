0cd3b3be8b2d8f1b83a5f1218daa5264f552e165
Coffee Cloud Consulting <157412144+coffee-cloud-consulting@users.noreply.github.com>
2024-07-29 21:30:50 -0300

    fix(vars)

--- examples/sample/main.tf
+++ examples/sample/main.tf
@@ -1,10 +1,17 @@
 locals {
   region              = "us-east-2"
   name                = "quarks-labs"
-  cidr                = "10.3.0.0/16"
+  cidr                 = "10.3.0.0/16"
+  wait_for_ready_timeout             =  "40min"
   solution_stack_name = "64bit Amazon Linux 2023 v4.3.4 running Docker "
   private_subnets     = ["10.3.32.0/19", "10.3.96.0/19"]
   public_subnets      = ["10.3.64.0/19", "10.3.128.0/19"]
+  enable_nat_gateway                   = true
+  single_nat_gateway                   = true
+  enable_dns_hostnames                 = true
+  enable_flow_log                      = true
+  create_flow_log_cloudwatch_iam_role  = true
+  create_flow_log_cloudwatch_log_group = true
 }
 
 
@@ -20,12 +27,12 @@ module "vpc" {
   azs                                  = ["${local.region}a", "${local.region}b"]
   private_subnets                      = local.private_subnets
   public_subnets                       = local.public_subnets
-  enable_nat_gateway                   = true
-  single_nat_gateway                   = true
-  enable_dns_hostnames                 = true
-  enable_flow_log                      = true
-  create_flow_log_cloudwatch_iam_role  = true
-  create_flow_log_cloudwatch_log_group = true
+  enable_nat_gateway                   = local.enable_nat_gateway
+  single_nat_gateway                   = local.single_nat_gateway
+  enable_dns_hostnames                 = local.enable_dns_hostnames
+  enable_flow_log                      = local.enable_flow_log
+  create_flow_log_cloudwatch_iam_role  = local.create_flow_log_cloudwatch_iam_role
+  create_flow_log_cloudwatch_log_group = local.create_flow_log_cloudwatch_log_group
 }
 
 ###########################################################
@@ -38,11 +45,11 @@ module "elastic_beanstalk" {
   name                   = local.name
   solution_stack_name    = local.solution_stack_name
   region                 = local.region
+  wait_for_ready_timeout = local.wait_for_ready_timeout
   vpc_id                 = module.vpc.default_vpc_id
   azs                    = module.vpc.azs
   private_subnets        = module.vpc.private_subnets
   public_subnets         = module.vpc.public_subnets
-  wait_for_ready_timeout = "40min"
   cert_manager           = []
   custom_settings = [{
 
@@ -78,7 +85,7 @@ module "elastic_beanstalk" {
     {
       namespace = "aws:rds:dbinstance"
       name      = "DBPassword"
-      value     = "b4nclim4p4ss"
+      value     = "el4sticdbpass"
     },
     {
       namespace = "aws:rds:dbinstance"
